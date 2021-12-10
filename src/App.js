4e3c27c593636461aa8a7d1c72f6f51ce7958ba8
Tsuyoshi Kiryu <ainto0199@gmail.com>
2021-12-10 13:32:37 +0900

    React interactivity: Events and state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from './components/Form';
 import FilterButton from './components/FilterButton';
 import Todo from "./components/Todo";
-
+import { nanoid } from "nanoid";
 
 function App(props) {
-  const taskList = props.tasks.map(task => (
-    <Todo id={task.id} name={task.name} completed={task.completed} key={task.id} />
+  const [tasks, setTasks] = useState(props.tasks);
+  function addTask(name) {
+    const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map(task => {
+      if (id === task.id) {
+        return { ...task, completed: !task.completed }
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter(task => id !== task.id);
+    setTasks(remainingTasks);
+  }
+  const taskList = tasks.map(task => (
+    <Todo id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+    />
   ));
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form />
+      <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
         <FilterButton />
         <FilterButton />
         <FilterButton />
       </div>
       <h2 id="list-heading">
-        3 tasks remaining
+        {headingText}
       </h2>
       <ul
         role="list"
